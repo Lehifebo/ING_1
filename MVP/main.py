135a6247a124eb98ab1a8e50c9f57a112ca807eb
tinosauro <m.pelucchi@student.rug.nl>
2023-04-19 15:13:39 +0200

    email list saved to a file

--- MVP/main.py
+++ MVP/main.py
@@ -3,9 +3,14 @@ import convert_To_Pivot as ctv
 import map_Groups as mg
 import merge_Tables as mt
 import generateEmails as ge
-files,path = fr.getExcels()
-tables=mg.applyDictionary(ctv.toPivotTable(files,path))
-merged_table= mt.merge(tables,'CI Config Admin Group')
+files, path = fr.getExcels()
+tables = mg.applyDictionary(ctv.toPivotTable(files, path))
+merged_table = mt.merge(tables,'CI Config Admin Group')
 merged_table.fillna(0, inplace=True)
 #print(merged_table)
-print(ge.generateEmails(merged_table))
+#print(ge.generateEmails(merged_table))
+path = "emailString.txt" #can be changed to another folder
+f = open(path, "w")
+f.write(ge.generateEmails(merged_table))
+f.close()
+
