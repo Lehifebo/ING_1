6349bd9dea943ec41f797987397b1220830fc04e
tinosauro <m.pelucchi@student.rug.nl>
2023-05-03 13:40:45 +0200

    deleted useless files

--- MVP/merge_Tables.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-def merge(tables,collumn):
-    merged_table=pd.DataFrame(columns=[collumn])
-    for table in tables:
-        merged_table = pd.merge(merged_table, table, on=collumn, how='outer')
-    return merged_table
